0346a472c79dacc88cd4e86ad57565c11cbdc85d
Carlos Cabrera <crafael@cubatel.cu>
2016-06-29 12:31:24 -0400

    ep3 ch7 broken ship

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/lib/Model/AbstractShip.php';
 require_once __DIR__ . '/lib/Model/Ship.php';
 require_once __DIR__ . '/lib/Model/RebelShip.php';
+require_once __DIR__ . '/lib/Model/BrokenShip.php';
 require_once __DIR__ . '/lib/Model/BattleResult.php';
 require_once __DIR__ . '/lib/Service/Container.php';
 require_once __DIR__ . '/lib/Service/BattleManager.php';
